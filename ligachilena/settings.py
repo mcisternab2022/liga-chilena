1b8563619b699eb410c8fb2a9a99cc4f75ede432
Marcos <m.cisternab2022@gmail.com>
2023-02-06 19:44:01 -0300

    cambios

--- ligachilena/settings.py
+++ ligachilena/settings.py
@@ -80,7 +80,10 @@ WSGI_APPLICATION = 'ligachilena.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
