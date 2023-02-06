9f3695b157cce44815a1eabf1a3d57df539bcaf3
Marcos <m.cisternab2022@gmail.com>
2023-02-06 20:56:20 -0300

    cambio bd

--- ligachilena/settings.py
+++ ligachilena/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'ligachilena.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
