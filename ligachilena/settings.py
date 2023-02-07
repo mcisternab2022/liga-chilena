256171f399b74fa1a3a6fcaff0acc9e926d7fc5a
Marcos <m.cisternab2022@gmail.com>
2023-02-07 15:22:15 -0300

    Cambio de escudos

--- ligachilena/settings.py
+++ ligachilena/settings.py
@@ -141,15 +141,6 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
 
 MEDIA_URL = 'media/'
 
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-
 LOGIN_URL = '/login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
