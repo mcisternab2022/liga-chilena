afa80e85d3b4f466f5e818c546c452a6cff72406
Marcos <m.cisternab2022@gmail.com>
2023-02-13 12:27:18 -0300

    Se hace responsive la página

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Equipo(models.Model):
   nombre_equipo = models.CharField(max_length=50)
   imagen = models.ImageField(upload_to="escudos", null=True)
+  pagina = models.CharField(max_length=150)
 
   def __str__(self):
       return self.nombre_equipo
